4b1129f79bf7cd6db281f3e36e6f6dbed3e7afda
BradSissons <b.sissons98@gmail.com>
2021-08-27 23:47:24 +1200

    Added store items and amended createOrder

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
-
 const app = express()
 app.set("view engine", "ejs")
 app.use(express.static("public"))
+
+const storeItems = new Map([
+	[1, { price: 100, name: "Item 1" }],
+	[2, { price: 200, name: "Item 2" }]
+])
 
 app.get('/', (req, res) => {
 	res.render('index', { paypalClientId: 'AbX7relgVWWbg56Y7qC5-uvM0KG7lh1z_zx0xTM0KmCEMDH9bUwLDCgYPSFbYhgB1odFsk24yr7n826g'})
